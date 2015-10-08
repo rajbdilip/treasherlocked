e419820dd6119f2c66b6d0bd72c826b4f4cc64a2
sangeet-singh <sangeetsingh123@gmail.com>
2015-10-08 11:26:23 +0530

    Modify add-question.php

--- add-question.php
+++ add-question.php
@@ -5,6 +5,7 @@
 	
 	require_once( DOCUMENT_ROOT . 'classes/LoginHelper.php' );
 	require_once( DOCUMENT_ROOT . 'config/db.php' );
+	require(DOCUMENT_ROOT . 'includes/html/add_question.php');
 	
 	$loginHelper = new LoginHelper( $db );
 	if ( $loginHelper->IsLoggedIn() ) {
@@ -17,67 +18,6 @@
 	$_SESSION['spoof_proof'] = $spoof_proof;
 ?>
 
-<?php
-	if(isset($_POST['submit']))
-	{
-		$upload_ok=1;
-		if($_FILES['favicon']['name']!='')
-		{
-			$target_dir = $_SERVER['DOCUMENT_ROOT'] . "/ts2/_static/img/favicon/"; 
-			$target_file = $target_dir . basename($_FILES['favicon']['name']);
-			if(strlen($_FILES['favicon']['name'])>20)	//checks the filename string size
-			{
-				$error = "File name is too large. It should be less than 20 characters.";
-				$upload_ok = 0;
-			}
-			$check = getimagesize($_FILES['favicon']['tmp_name']);
-			if($check['mime'] != "image/jpeg" && $check['mime'] !="image/png")	//checks if the file is an image.
-			{
-				$error = "The file type is not supported.";
-				$upload_ok = 0;
-			}			
-			if($_FILES['favicon']['size'] > 500000)	//checks filesize < 500 KB
-			{
-				$error = "The file size is too large.";
-				$upload_ok = 0;
-			}
-			if(!move_uploaded_file($_FILES['favicon']['tmp_name'], $target_file))	//checks error in file upload
-			{
-				$error = "error uploading image.";
-				$upload_ok = 0;
-			}
-		}
-		if($upload_ok === 1)
-		{
-			$db->where('level', $_POST['level']);
-			$db->getOne('questions');
-			if($db->count > 0)
-			{
-				$error = "Question for this level already exists.";
-				$upload_ok = 0;
-			}
-			$data['level'] = $_POST['level'];
-			$data['html'] = $_POST['html'];
-			$data['answer'] = sha1($_POST['answer']);			
-			if(isset($_POST['url_mask']))
-			{
-				$data['url_mask'] = $_POST['url_mask'];
-			}
-			if($_FILES['favicon']['name']!= '' && $upload_ok == 1)
-			{
-				$data['favicon'] = $_FILES['favicon']['name'];
-			}
-			if($upload_ok != 0)
-			{
-				$query = $db->insert('questions', $data);
-				if($query === 0)
-				{
-					$success = true;
-				}
-			}
-		}
-	}
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -111,21 +51,15 @@
 		<div class="container">
 			<div class="row">
 				<?php
-					if(isset($_POST['submit']))
+					if(isset($success))
+					{	
+						echo "<p>Question Added Successfully.</p><br />
+						<a href=\"". $_SERVER["PHP_SELF"] . "\" class=\"btn btn-primary\">Add Another Question</a>";
+					}
+					if(isset($error))
 					{
-						if(isset($success))
-						{	
-							if($success === true)
-							{
-								echo "<p>Question Added Successfully.</p><br />
-									<a href=\"". $_SERVER["PHP_SELF"] . "\" class=\"btn btn-primary\">Add Another Question</a>";
-							}
-						}
-						if(isset($error))
-						{
-							echo "<p>".$error ."</p>". 
-							"<br /><a href=\"". $_SERVER["PHP_SELF"] . "\" class=\"btn btn-primary\">Try Again</a>";
-						}
+						echo "<p>".$error ."</p>". 
+						"<br /><a href=\"". $_SERVER["PHP_SELF"] . "\" class=\"btn btn-primary\">Try Again</a>";
 					}
 				?>
 			</div>
---
+++ includes/html/add_question.php
@@ -0,0 +1,67 @@
+<?php
+
+	require_once(dirname(dirname(dirname(__FILE__))) . '/config/consts.php');
+	require_once(dirname(dirname(dirname(__FILE__))) . '/config/db.php' );
+	if(isset($_POST['submit']))
+	{
+		$upload_ok=1;
+		if($_FILES['favicon']['name']!='')
+		{
+			$target_dir = $_SERVER['DOCUMENT_ROOT'] . "/ts2/_static/img/favicon/"; 
+			$target_file = $target_dir . basename($_FILES['favicon']['name']);
+			if(strlen($_FILES['favicon']['name'])>20)	//checks the filename string size
+			{
+				$error = "File name is too large. It should be less than 20 characters.";
+				$upload_ok = 0;
+			}
+			$check = getimagesize($_FILES['favicon']['tmp_name']);
+			if($check['mime'] != "image/jpeg" && $check['mime'] !="image/png")	//checks if the file is an image.
+			{
+				$error = "The file type is not supported.";
+				$upload_ok = 0;
+			}			
+			if($_FILES['favicon']['size'] > 500000)	//checks filesize < 500 KB
+			{
+				$error = "The file size is too large.";
+				$upload_ok = 0;
+			}
+			if($upload_ok != 0)
+			{
+				if(!move_uploaded_file($_FILES['favicon']['tmp_name'], $target_file))	//checks error in file upload
+				{
+					$error = "error uploading image.";
+					$upload_ok = 0;
+				}
+			}
+		}
+		if($upload_ok === 1)
+		{
+			$db->where('level', $_POST['level']);
+			$db->getOne('questions');
+			if($db->count > 0)
+			{
+				$error = "Question for this level already exists.";
+				$upload_ok = 0;
+			}
+			$data['level'] = $_POST['level'];
+			$data['html'] = $_POST['html'];
+			$data['answer'] = sha1($_POST['answer']);			
+			if(isset($_POST['url_mask']))
+			{
+				$data['url_mask'] = $_POST['url_mask'];
+			}
+			if($_FILES['favicon']['name']!= '' && $upload_ok == 1)
+			{
+				$data['favicon'] = $_FILES['favicon']['name'];
+			}
+			if($upload_ok != 0)
+			{
+				$query = $db->insert('questions', $data);
+				if($query === 0)
+				{
+					$success = 1;
+				}
+			}
+		}
+	}
+?>
